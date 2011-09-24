020d03085f23af44f25c1fd22645b17bed6ed59b
Sungho Cho <sungho.cho@sv.cmu.edu>
2011-09-24 04:06:53 -0400

    Updated signup page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Return a image tag for the logo
+  def logo
+    image_tag("logo.png", :alt => "Read-O-Meter", :class => "round")
+  end
+
   # Return a title on a per-page basis.
   def title
     base_title = "Read-O-Meter: Track your reading"
