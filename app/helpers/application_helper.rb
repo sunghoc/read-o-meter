a0853f99b312f5291ed9a8c7a561811d1d8aaf13
Sungho Cho <sungho.cho@sv.cmu.edu>
2011-09-24 03:48:07 -0400

    Updated pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "Read-O-Meter: Track your reading"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
