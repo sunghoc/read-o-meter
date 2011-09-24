bf9edde4a19aeec2e27bae014db7b8d8c2aa7dc0
Sungho Cho <sungho.cho@sv.cmu.edu>
2011-09-24 02:41:23 -0400

    Initial Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReadOMeter::Application.config.secret_token = 'f72b9b4e78aff6d11d22170c88b9e807fefca7c8080d779f4caf2bc068cd1c3289e4519d54970e68fbdb4f535221d23746c198bb24e5ea048ab6a47bfb5539c2'
